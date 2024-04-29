e9aa726899009c4226759aac7a161ac62d64b3f0
minionsgithub <minions.goodboss@gmail.com>
2024-04-29 21:46:52 +0700

    myRecipeApp

---
+++ app/src/main/java/vn/tutorials/myrecipeapp/RecipeApp.kt
@@ -0,0 +1,33 @@
+package vn.tutorials.myrecipeapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun RecipeApp(navController: NavHostController) {
+    val recipeViewModel: MainViewModel = viewModel()
+    val viewState by recipeViewModel.categoriesState
+
+    NavHost(
+        navController = navController, startDestination = Screen.RecipeScreen.route
+    ) {
+        composable(route = Screen.RecipeScreen.route) {
+            RecipeScreen(viewState = viewState, navigateToDetail = {
+                // This part is responsible for passing data from the current screen to the detail screen
+                // It utilizes the savedStateHandle, which is a component of the Compose navigation framework
+                navController.currentBackStackEntry?.savedStateHandle?.set("category", it)
+                navController.navigate(Screen.DetailScreen.route)
+            })
+        }
+        composable(route = Screen.DetailScreen.route) {
+            val category =
+                navController.previousBackStackEntry?.savedStateHandle?.get<Category>("category")
+                    ?: Category("", "", "", "")
+            CategoryDetailScreen(category = category)
+        }
+    }
+}
---
+++ app/src/main/java/vn/tutorials/myrecipeapp/Screen.kt
@@ -0,0 +1,7 @@
+package vn.tutorials.myrecipeapp
+
+sealed class Screen(val route:String) {
+    data object RecipeScreen:Screen("recipe_screen")
+    data object DetailScreen:Screen("detail_screen")
+
+}
